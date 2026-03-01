4b75fa321589c138c6a2f8784bb0a23b1b6f7f93
DAM-Fran <al039103@alumnos.iesitacaalcorcon.org>
2026-03-01 19:36:26 +0100

    Add files via upload

---
+++ MauiPractica/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MauiPractica.Pages;
+
+namespace MauiPractica
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(DetalleEmpleadoPage), typeof(DetalleEmpleadoPage));
+        }
+    }
+}
---
+++ MauiPractica/MauiProgram.cs
@@ -0,0 +1,46 @@
+﻿using MauiPractica.PageModels;
+using MauiPractica.Pages;
+using MauiPractica.Services;
+using Microsoft.Extensions.Logging;
+
+namespace MauiPractica
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+            //builder.Services.AddSingleton<ILoggerFactory>();  // aquí se inyectan las dependencias
+#endif
+            builder.Services.AddTransient<MainPage>();
+
+            builder.Services.AddSingleton<IApiService, ApiService>();
+
+            builder.Services.AddTransient<EmpleadosPageModel>();
+            builder.Services.AddTransient<EmpleadosPage>();
+            builder.Services.AddTransient<DepartamentosPageModel>();
+            builder.Services.AddTransient<DepartamentosPage>();
+            builder.Services.AddTransient<DetalleEmpleadoPage>();
+            builder.Services.AddTransient<DetalleEmpleadoPageModel>();
+
+            /*
+            builder.Services.AddTransient<MainPageModel>();
+            builder.Services.AddTransient<AboutPage>();
+            builder.Services.AddTransient<DetailPage>();
+            builder.Services.AddTransient<DetailPageModel>();
+            */
+
+            return builder.Build();
+        }
+    }
+}
