119e42910965eb221c1c35248f2701d498fc27a2
DAM-Fran <al039103@alumnos.iesitacaalcorcon.org>
2026-03-01 19:34:01 +0100

    Add files via upload

---
+++ MauiPractica/Services/ApiService.cs
@@ -0,0 +1,25 @@
+﻿using MauiPractica.Models;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace MauiPractica.Services;
+
+public class ApiService : IApiService
+{
+    HttpClient _client = new HttpClient();
+    JsonSerializerOptions _serializerOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+    Uri baseUri = new(string.Format("http://127.0.0.1:8000"));
+
+    public async Task<List<Empleado>> GetEmpleadosAsync() => await _client.GetFromJsonAsync<List<Empleado>>(baseUri + "/empleados/");
+    /*
+    public async Task<List<Empleado>> GetEmpleadosAsync()
+    {
+        return new List<Empleado>
+    {
+        new Empleado { Id = 1, Nombre = "Test" }
+    };
+    }
+    */
+
+    public async Task<List<Departamento>> GetDepartamentosAsync() => await _client.GetFromJsonAsync<List<Departamento>>(baseUri + "/departamentos/");
+}
---
+++ MauiPractica/Services/IApiService.cs
@@ -0,0 +1,9 @@
+﻿using MauiPractica.Models;
+
+namespace MauiPractica.Services;
+
+public interface IApiService
+{
+    public Task<List<Empleado>> GetEmpleadosAsync();
+    public Task<List<Departamento>> GetDepartamentosAsync();
+}
